749149be2c767a52e099b779d3ef76d5c4f1ac7a
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-02-27 02:43:20 -0600

    build: add eslint dependencies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express'
+import bodyParser from 'body-parser'
 
-const app = express();
-app.set("port", process.env.PORT || 3000);
+const app = express()
+app.set('port', process.env.PORT || 3000)
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.listen(app.get("port"), () => {
-  console.log(`Server on port ${app.get("port")}`);
-});
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`)
+})
