f07e9d0fab210dee79c3b862b4751553bd59fa05
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-02-27 03:20:37 -0600

    build: add axios dependencies

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,27 +1,5 @@
-import { Cheerio } from 'cheerio'
+import * as cheerio from 'cheerio'
 
 export const getWordDay = (req, res) => {
-  const options = {
-    uri: 'https://www.dictionary.com/e/word-of-the-day/',
-    transform: function (body) {
-      return Cheerio.load(body)
-    }
-  }
-
-  request(options)
-    .then(function ($) {
-      const word = $('.js-fit-text:first')
-      const wordData = $('.otd-item-headword__pos p')
-      const category = $(wordData).eq(0).text().trim()
-      const meaning = $(wordData).eq(1).text().trim()
-
-      console.log(word.text())
-      console.log(category)
-      console.log(meaning)
-    })
-    .catch(function (err) {
-      console.log(err)
-    })
-
-  res.json({ Title: 'Hello World' })
+  res.json({ Title: 'Hello' })
 }
