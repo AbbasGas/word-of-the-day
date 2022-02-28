6c64b013546e7a1ce68753b116161728ab7b8d1d
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-02-28 13:20:26 -0600

    feat: add date, pronunciation and audio to json response

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -12,14 +12,20 @@ export const getWordDay = async (req, res) => {
 
     const $ = cheerio.load(data)
 
-    const word = $('.js-fit-text:first')
+    const date = $('.otd-item-headword__date:first').text().trim()
+    const pronunciation = $('.otd-item-headword__pronunciation__text:first').text().trim()
+    const audio = $('.otd-item-headword__pronunciation-audio:first').attr('href')
+    const word = $('.js-fit-text:first').text()
     const wordData = $('.otd-item-headword__pos p')
     const category = $(wordData).eq(0).text().trim()
     const meaning = $(wordData).eq(1).text().trim()
 
     res.json({
       status: 200,
-      word: word.text(),
+      date: date,
+      pronunciation: pronunciation,
+      audio: audio,
+      word: word,
       category: category,
       meaning: meaning
     })
